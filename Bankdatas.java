d43d83d255562155727cfb379c2338e04bfe591e
Mugilan205 <152079129+Mugilan205@users.noreply.github.com>
2024-11-11 19:20:20 +0530

    Bankdatas.java

---
+++ Bankdatas.java
@@ -0,0 +1,16 @@
+package databasee;
+
+public class Bankdatas {
+	
+	String BankName ;
+	int BankId  ;
+    String Branch ;
+	
+   public Bankdatas(int Id ,String BName ,String Bch ) {
+	   BankName =BName;
+	   BankId = Id;
+	   Branch = Bch;
+		
+   }
+	
+}
