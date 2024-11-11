61dbe53e052ac15d4fa201ca9c5805ae23918e4f
Mugilan205 <152079129+Mugilan205@users.noreply.github.com>
2024-11-11 19:23:14 +0530

    Create SavingsAcc.java

---
+++ SavingsAcc.java
@@ -0,0 +1,14 @@
+package databasee;
+
+public class SavingsAcc {
+  int acc_id;
+  float int_rate ;
+   
+  public SavingsAcc( int acc_id,
+  float int_rate ) {
+	  this.acc_id=acc_id;
+	  this.int_rate =int_rate ;
+	  
+  }
+  
+}
