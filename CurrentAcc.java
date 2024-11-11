9bc8bbc3237d264659e8f18193716a016733c89c
Mugilan205 <152079129+Mugilan205@users.noreply.github.com>
2024-11-11 19:21:15 +0530

    CurrentAcc.java

---
+++ CurrentAcc.java
@@ -0,0 +1,12 @@
+package databasee;
+
+public class CurrentAcc {
+   int acc_id;
+   float od_limit;
+   public CurrentAcc(int acc_id,
+   float od_limit) {
+	   this. acc_id = acc_id;
+	   this. od_limit = od_limit;
+   }
+   
+}
