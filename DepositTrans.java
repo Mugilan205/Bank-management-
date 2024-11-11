96ec249adbef7a7d777574bea6452c1016ccb5db
Mugilan205 <152079129+Mugilan205@users.noreply.github.com>
2024-11-11 19:22:45 +0530

    Create DepositTrans.java

---
+++ DepositTrans.java
@@ -0,0 +1,13 @@
+package databasee;
+
+public class DepositTrans {
+    int trans_id ;
+    String trans_method;
+    
+    public DepositTrans(int trans_id , String trans_method) {
+    	  this.trans_id = trans_id ;
+    	  this .trans_method = trans_method ;
+    	  
+    }
+	
+}
